c17f8e0c1a0f4b2d7e5585144cbc2615fabcc316
milanp <milan.dudhat@cybercom.in>
2023-12-18 18:37:32 +0530

    env changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config();
-const port = process.env.PORT || 3030;
+const port = process.env.PORT;
 const app = express();
 const cors = require('cors');
 const fs = require('fs');
